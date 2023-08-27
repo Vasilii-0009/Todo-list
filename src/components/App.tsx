b979301cf952509072c3c2b5310c963dbbda0405
Василий Морарь <vasili-morari@MacBook-Air-Vasilij.local>
2023-08-27 08:16:17 +0300

    change-package

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,13 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
 import { Header } from "./Header/Headet";
 import { TodoListPage } from "./TodoListPage/TodoListPage";
-import { AboutePage } from "./AboutePage/AboutePage";
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
       <div className="wrapper">
-        <Routes>
-          <Route path="/" element={<TodoListPage />} />
-          <Route path="/aboute" element={<AboutePage />} />
-        </Routes>
+        <TodoListPage />
       </div>
     </>
   );
